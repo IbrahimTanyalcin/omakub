5adbeaab867dfa1fa9c4e16867ceecad6ff169fc
Ibrahim <8837895+IbrahimTanyalcin@users.noreply.github.com>
2024-07-18 19:13:48 +0000

    enable arch aliases, remove hardcoded x86_64

--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -1,3 +1,5 @@
+source ~/.local/share/omakub/get_arch.sh
+ARCH=$(get_arch "dockercompose")
 sudo apt install -y docker.io docker-buildx
 sudo usermod -aG docker ${USER}
 
@@ -7,5 +9,6 @@ echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' |
 DOCKER_COMPOSE_VERSION="2.27.0"
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -sSL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+if curl -sSL "https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-${ARCH}" -o "$DOCKER_CONFIG/cli-plugins/docker-compose"; then
+    chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+fi
